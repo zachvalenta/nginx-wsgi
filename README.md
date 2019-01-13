9a644ee649cd1561186d0e105817a09e4c407171
zachvalenta <zjv5002@gmail.com>
2019-01-13 16:44:50 -0500

    add README

---
+++ README.md
@@ -0,0 +1,11 @@
+# OVERVIEW
+
+# install
+
+* just follow the Makefile
+
+📝 I use `httpie` to hit the servers, but `curl` or a browser work just as well
+
+## todo
+
+* update to use `httpie` instead of `qiu`
--- myproject.py
+++ myproject.py
@@ -1,9 +1,11 @@
 from flask import Flask
 application = Flask(__name__)
 
+
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return "<h1 style='color:blue'>Flask running</h1>"
+
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
